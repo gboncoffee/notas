007a3965737b25a2087d8ab689f737b096a1ede8
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2022-12-17 17:41:07 -0300

    Adicionadas notas sobre documentação em Rust

---
+++ Rust/documentacao.rs
@@ -0,0 +1,18 @@
+///
+/// # Comentários de documentação são feitos com três barras.
+///
+/// Como **pode-se ver**, eles suportam Markdown
+///
+/// ```
+/// println!("Então é bem fácil criar exemplos e etc!");
+/// ```
+///
+fn example() {
+    println!("Exemplo!");
+}
+
+//!
+//! Comentários de documentação com //! servem para documentar o que os contém ao invés do que vem
+//! logo em seguida. Normalmente usados em src/lib.rs para documentar a crate ou o módulo como um
+//! todo.
+//!
